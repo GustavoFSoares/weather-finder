870918ef43ef042c699fd10f06bbecb76ebd3fd7
Gustavo Soares <gustavo.fe.soares@gmail.com>
2024-08-23 23:55:42 -0300

    feat: added main layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  css: ["~/assets/css/tailwind.css"],
-  modules: [
-    "@nuxtjs/tailwindcss",
-    "@nuxt/eslint",
-    "vuetify-nuxt-module",
-  ],
+  css: ["~/assets/css/tailwind.css", "~/assets/css/main.scss"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint", "vuetify-nuxt-module"],
 });
