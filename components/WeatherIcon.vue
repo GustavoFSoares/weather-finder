eec532c5d728109c9accfac6acfe646bd6ff2bd4
Gustavo Soares <gustavo.fe.soares@gmail.com>
2024-08-27 21:31:22 -0300

    feat: created weatherIcon component

---
+++ components/WeatherIcon.vue
@@ -0,0 +1,40 @@
+<template>
+  <component :is="mappedIcon" filled />
+</template>
+
+<script lang="ts" setup>
+type WeatherIcons =
+  | "clould"
+  | "party-clould"
+  | "rain-thunder"
+  | "rainy"
+  | "snowy"
+  | "sunny";
+type Props = {
+  name: string;
+};
+
+const props = defineProps<Props>();
+
+const mappedIcon = computed(() => {
+  const prefix = "svgo-";
+
+  let icon: WeatherIcons | null = null;
+
+  switch (props.name) {
+    case "clouds":
+      icon = "clould";
+      break;
+
+    case "clear":
+      icon = "sunny";
+      break;
+
+    default:
+      icon = "sunny";
+      break;
+  }
+
+  return prefix + icon;
+});
+</script>
